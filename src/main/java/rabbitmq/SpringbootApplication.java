1699939ec297b77bb798639a461517ffb48395cf
zjqwll <85583755@qq.com>
2017-07-04 15:28:10 +0800

    代码提交

--- src/main/java/rabbitmq/SpringbootApplication.java
+++ src/main/java/rabbitmq/SpringbootApplication.java
@@ -1,16 +1,39 @@
 package rabbitmq;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import rabbitmq.work.RabbitAmqpTutorialsRunner;
 
 @SpringBootApplication
-public class SpringbootApplication{ 
-    
-    public static void main(String[] args) {
-      	SpringApplication app = new SpringApplication(SpringbootApplication.class);
-		app.setAddCommandLineProperties(false);//屏蔽命令行访问属性的设置
-		app.run(args);
-    	System.out.println("totoro manage server is started!");
+@EnableScheduling
+public class SpringbootApplication {
+
+    @Profile("usage_message")
+    @Bean
+    public CommandLineRunner usage() {
+        return new CommandLineRunner() {
+
+            @Override
+            public void run(String... arg0) throws Exception {
+                System.out.println("This app uses Spring Profiles to control its behavior.\n");
+                System.out.println("Sample usage: java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,sender");
+            }
+        };
+    }
+
+    @Profile("!usage_message")
+    @Bean
+    public CommandLineRunner tutorial() {
+        return new RabbitAmqpTutorialsRunner();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 }
 
---
+++ src/main/java/rabbitmq/work/Tut2Config.java
@@ -0,0 +1,36 @@
+package rabbitmq.work;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"tut2", "work-queues"})
+@Configuration
+public class Tut2Config {
+
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    private static class ReceiverConfig {
+
+        @Bean
+        public Tut2Receiver receiver1() {
+            return new Tut2Receiver(1);
+        }
+
+        @Bean
+        public Tut2Receiver receiver2() {
+            return new Tut2Receiver(2);
+        }
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut2Sender sender() {
+        return new Tut2Sender();
+    }
+}
